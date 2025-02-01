9f71fb8269781176e3eec1f2b09dcdf542268d6c
piotrtrybus <125976101+piotrtrybus@users.noreply.github.com>
2025-02-01 17:29:44 +0100

    Update chunks.py

--- chunks.py
+++ chunks.py
@@ -1,13 +1,17 @@
 import pandas as pd
 
-chunk_size = 10_000
+chunk_size = 10000
+
+chunk_count = 0
 
 for chunk in pd.read_csv("Crime_Data_from_2020_to_Present.csv",chunksize=chunk_size):
 
     filtered_chunk = chunk[chunk["Crm Cd Desc"] == "VEHICLE - STOLEN"]
 
     filtered_chunk.to_csv("stolen_vehicles.csv", mode="a", index=False, header=False)
-    
+
+    chunk_count += 1
+
     print(f"{chunk} processed")
 
-print("All data processed")
+print(f"All data processed through {chunk_count} chunks")
